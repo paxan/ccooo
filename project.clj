c0bae42ee2c51f35739262526fb8ac13d9cc98e1
John Wiseman <jjwiseman@gmail.com>
2014-04-19 15:42:36 -0700

    Fix hadoop dependency.
    hadoop-core is not just a dev-time dependency.  Without this, `lein uberjar` from a clean checkout fails.

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :uberjar-name "ccooo-standalone.jar"
   :dependencies [[cascalog "1.10.0"]
                  [cascalog-more-taps "0.3.0"]
+                 [org.apache.hadoop/hadoop-core "1.0.3"]
                  [org.clojure/data.json "0.1.3"]
                  [commons-httpclient "3.0.1"]]
   :profiles {:dev
-             {:dependencies [[org.apache.hadoop/hadoop-core "1.0.3"]
-                             [midje "1.4.0"]
+             {:dependencies [[midje "1.4.0"]
                              [midje-cascalog "0.4.0" :exclusions [org.clojure/clojure]]]
               :plugins [[lein-midje "2.0.0"]]}}
   :aot [ccooo.commoncrawl]
